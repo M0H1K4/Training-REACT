9380961f847946b15b06f298da3837d265067b97
M0H1K4N <nejer911@gmail.com>
2024-04-05 14:52:49 +0400

    add

--- src/App.jsx
+++ src/App.jsx
@@ -13,16 +13,16 @@ function HandleClickAlert(){
   );
 }
 
-// function MyButton() {
-//   const [count, setCount] = useState(0);
+function MyButton() {
+  const [count, setCount] = useState(0);
 
-//   const HandleClick = () => {
-//     setCount(count + 1);
-//   };
-//   return <>
-//     <button onClick={HandleClick}>Clicked {count} times</button>
+  const HandleClick = () => {
+    setCount(count + 1);
+  };
+  return <>
+    <button onClick={HandleClick}>Clicked {count} times</button>
 
-//   </>;
-// }
+  </>;
+}
 
 export default App;
